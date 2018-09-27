7e5a7fcd416353216489b322d8e7612f829769a8
Yevhenii Moroziuk <yevhenii.moroziuk@dell31.office.quintagroup.com>
2018-09-27 17:10:23 +0300

    Update structure

--- SGC/_build/make.bat
+++ SGC/_build/make.bat
@@ -5,9 +5,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set BUILDDIR=/home/yevhenii.moroziuk/Repository/tor/docs/_build
-set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% /home/yevhenii.moroziuk/Repository/tor/docs/source
-set I18NSPHINXOPTS=%SPHINXOPTS% /home/yevhenii.moroziuk/Repository/tor/docs/source
+set BUILDDIR=/home/yevhenii.moroziuk/Repository/tor/SGC/_build
+set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% /home/yevhenii.moroziuk/Repository/tor/SGC/source
+set I18NSPHINXOPTS=%SPHINXOPTS% /home/yevhenii.moroziuk/Repository/tor/SGC/source
 if NOT "%PAPER%" == "" (
 	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
 	set I18NSPHINXOPTS=-D latex_paper_size=%PAPER% %I18NSPHINXOPTS%
@@ -108,9 +108,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\docs.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\docs_SGC.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\docs.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\docs_SGC.ghc
 	goto end
 )
 
